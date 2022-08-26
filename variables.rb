a5753415931c36b55c18e84eb25333d53fd78cfb
Issa Abdel Mageed <imageed95@gmail.com>
2022-08-25 21:46:22 -0600

    fixed strings and added puts statements

--- variables.rb
+++ variables.rb
@@ -1,18 +1,31 @@
 # variables
 
-email = issatest@gmail.com
-name = issa
-password = icethingypassword
+email = "issatest@gmail.com"
+name = "issa"
+password = "icethingypassword"
+puts email
+puts name
+puts password
+
 
 
 price = 50
 number_of_people = 43
 maximum_occupancy = 100
+puts price
+puts number_of_people
+puts maximum_occupancy
 
 bank_account = 150.35
 weight = 165.5
 speed = 65.3
+puts bank_account
+puts number_of_people
+puts speed
 
 payment = false
 complete_project = true
 accepted_terms_of_service = false
+puts payament
+puts complete_project
+puts accepted_terms_of_service
